d953b82f4bbf926862e1d6b2fbe4724a9899844f
Sladjana Jovicic <janey.webdev@gmail.com>
2023-03-03 16:58:36 +0100

    exercise 7 - http-client

---
+++ http-client.js
@@ -0,0 +1,11 @@
+'use strict'
+const http = require('http');
+
+const url = process.argv[2];
+
+http.get(url, function (res) {
+    res.setEncoding('utf8');
+
+    res.on('data', console.log);
+    res.on('error', console.error);
+}).on('error', console.error);
